62781991ebc0827d91a11d4a08fe2bd229de1ce9
jilanfawwaz <jilanfawwaz@gmail.com>
2022-05-25 23:26:14 +0800

    seatCubit : opsi lain untuk function isSelected

--- lib/cubit/seat_cubit.dart
+++ lib/cubit/seat_cubit.dart
@@ -7,12 +7,22 @@ class SeatCubit extends Cubit<List<String>> {
   SeatCubit() : super([]);
 
   bool isSelected(String id) {
-    if (state.indexOf(id) == -1) {
+    /*if (state.indexOf(id) == -1) {
+      return false;
+    } else {
+      return true;
+    }*/
+
+    int index = state.indexOf(
+        id); //mencari lokasi index id di dalam list, kalau misal gak ada maka nilai akan jadi -1 karena list dimulai dari index 0
+
+    if (index == -1) {
       return false;
     } else {
       return true;
     }
   }
+
 
   void selectSeat(String id) {
     print("state sebelumnya : $state");
